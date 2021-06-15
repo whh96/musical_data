a59337e6e6357b0d8fd33ffc3a3c7a14c416f29f
whh96 <66960104+whh96@users.noreply.github.com>
2021-06-15 12:53:22 -0500

    Add files via upload

--- musical_data.R
+++ musical_data.R
@@ -41,10 +41,59 @@ bb_bigram_count %>%
   ylab('Share of total chords') +
   xlab('chord changes')
 
+#######
 
+head(bb)
+bb_30_artists <- bb %>%
+  select(artist_compressed, title) %>%
+  unique() %>%
+  count(artist_compressed, sort = TRUE)
 
+bb_30_artists %>%
+  slice(1:30)
 
+tags <- tibble(
+  artist = c('Abba', 'Billy Joel', 'Elton John', 'Stevie Wonder', 'The Rolling Stones', 'The Beatles', 'Eric Clapton'),
+  instrument = c('piano', 'piano', 'piano', 'piano', 'guitar', 'guitar', 'guitar'))
 
+bb_tagged <- bb %>%
+  inner_join(tags)
+
+bb_tagged
+
+top_20 <- bb_count$chord[1:20]
+
+bb_tagged %>%
+  filter(chord %in% top_20) %>%
+  count(chord, instrument, sort = TRUE) %>%
+  ggplot(aes(chord,  n, fill = chord)) +
+  geom_col() +
+  facet_grid(~instrument) +
+  coord_flip() +
+  xlab('Total chords') +
+  ylab('Chord') + 
+  theme(legend.position = 'none')
+
+top_20_bigram <- bb_bigram_count$bigram[1:20]
+
+# The top 20 most common bigrams
+top_20_bigram <- bb_bigram_count$bigram[1:20]
+
+# Creating a faceted plot comparing guitar- and piano-driven songs for bigram frequency
+bb_tagged %>%
+  mutate(next_chord = lead(chord),
+         next_title = lead(title),
+         bigram = paste(chord, next_chord)) %>%
+  filter(title == next_title) %>%
+  count(bigram, instrument, sort = TRUE) %>%
+  filter(bigram %in% top_20_bigram) %>%
+  ggplot(aes(bigram, n, fill = bigram)) +
+  geom_col() +
+  facet_grid(~instrument) +
+  coord_flip() +
+  ylab('Total bigrams') +
+  xlab('Bigram') +
+  theme(legend.position="none")
 
 
 
